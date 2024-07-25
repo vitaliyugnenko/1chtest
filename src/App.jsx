3c11f9cf3b790c89391fd26e386d1e75a7e32d1f
Your Name <you@example.com>
2024-07-25 11:06:47 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Header from "./Header";
+import Swap from "./Swap";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <>
+      <div className="main-container">
+        <Header />
+        <Swap />
+        <Footer />
+      </div>
+    </>
+  );
+}
+
+export default App;
