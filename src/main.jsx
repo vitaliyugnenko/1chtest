fbf8ef0926b9f86b63201f97dc0031af13424b42
Your Name <you@example.com>
2024-07-30 15:26:28 +0300

    add wallets

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,34 @@ import { RainbowKitProvider, darkTheme } from "@rainbow-me/rainbowkit";
 
 import App from "./App.jsx";
 import "./index.css";
-import { config } from "./wagmi";
+//import { config } from "./wagmi";
+
+import { trustWallet } from "@rainbow-me/rainbowkit/wallets";
+import { uniswapWallet } from "@rainbow-me/rainbowkit/wallets";
+import { phantomWallet } from "@rainbow-me/rainbowkit/wallets";
+import { metaMaskWallet } from "@rainbow-me/rainbowkit/wallets";
+
+import { connectorsForWallets } from "@rainbow-me/rainbowkit";
+import { createConfig } from "wagmi";
+
+import { arbitrum, base, mainnet, optimism, polygon } from "wagmi/chains";
+
+const connectors = connectorsForWallets(
+  [
+    {
+      groupName: "Suggested",
+      wallets: [trustWallet, uniswapWallet, phantomWallet, metaMaskWallet],
+    },
+  ],
+  { appName: "RainbowKit App", projectId: "YOUR_PROJECT_ID" }
+);
+
+const config = createConfig({
+  connectors,
+  appName: "RainbowKit demo",
+  projectId: "90f079dc357f3b0a2500be0388582698",
+  chains: [mainnet, polygon, optimism, arbitrum, base],
+});
 
 const queryClient = new QueryClient();
 
@@ -25,6 +52,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
             display: "flex",
             flexDirection: "row",
           })}
+          locale="en"
         >
           <App />
         </RainbowKitProvider>
