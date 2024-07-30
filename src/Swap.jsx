cd7efb29d2bcab567c47fbb4aa82dc361582d955
Your Name <you@example.com>
2024-07-30 15:36:51 +0300

    change proxy link2

--- src/Swap.jsx
+++ src/Swap.jsx
@@ -143,12 +143,15 @@ function Swap({
       setLoading(true);
       setError(null);
       try {
-        const response = await axios.get("https://1chtest.vercel.app/crypto", {
-          params: {
-            symbol: selectedToken,
-            convert: "USD",
-          },
-        });
+        const response = await axios.get(
+          "https://1chtest.vercel.app/proxy/crypto",
+          {
+            params: {
+              symbol: selectedToken,
+              convert: "USD",
+            },
+          }
+        );
 
         // Логирование для отладки
         console.log("API Response:", response.data);
