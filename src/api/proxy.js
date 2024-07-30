3442e7796a506905093cb35dbd4e0d2c7abf4c99
Your Name <you@example.com>
2024-07-30 15:54:10 +0300

    change proxy

--- src/api/proxy.js
+++ src/api/proxy.js
@@ -1,37 +1,27 @@
-import express from "express";
-import request from "request";
+const request = require("request");
 
-const app = express();
-const port = 3000;
+export default function handler(req, res) {
+  if (req.method === "GET") {
+    const url = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=${req.query.symbol}&convert=${req.query.convert}`;
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
-
-app.get("/crypto", (req, res) => {
-  const url = `https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?symbol=${req.query.symbol}&convert=${req.query.convert}`;
-  request(
-    {
-      url: url,
-      headers: {
-        "X-CMC_PRO_API_KEY": "48d81810-7c07-454d-a289-436eeb9e9743",
+    request(
+      {
+        url: url,
+        headers: {
+          "X-CMC_PRO_API_KEY": "48d81810-7c07-454d-a289-436eeb9e9743",
+        },
       },
-    },
-    (error, response, body) => {
-      if (error) {
-        res.status(500).send(error);
-      } else {
-        res.send(body);
+      (error, response, body) => {
+        if (error) {
+          res.status(500).json({ error: "Internal Server Error" });
+        } else {
+          res.setHeader("Content-Type", "application/json");
+          res.status(200).send(body);
+        }
       }
-    }
-  );
-});
-
-app.listen(port, () => {
-  console.log(`Proxy server is running at http://localhost:${port}`);
-});
+    );
+  } else {
+    res.setHeader("Allow", ["GET"]);
+    res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+}
